720d5909b7e97b5df4700332182ecc9dd733ac4a
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-21 15:20:24 -0300

    Adicionando 4 horas do horário local.

--- src/programa/Hora.java
+++ src/programa/Hora.java
@@ -1,6 +1,7 @@
 package programa;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Hora {
@@ -10,8 +11,14 @@ public class Hora {
 		SimpleDateFormat SDF1 = new SimpleDateFormat("dd/MM/yyy HH:mm:ss ");
 
 		Date x1 = new Date();
+		Date x2 = new Date();
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(x2);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
 
 		System.out.println("Data atual x1: " + SDF1.format(x1));
+
 	}
 
 }
