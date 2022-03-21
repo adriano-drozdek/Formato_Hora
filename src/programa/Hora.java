e0e5e32787a98b68810248d9ab377f5a96a80a0b
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-21 15:22:59 -0300

    Atualizando e imprimindo novo horário.

--- src/programa/Hora.java
+++ src/programa/Hora.java
@@ -13,11 +13,15 @@ public class Hora {
 		Date x1 = new Date();
 		Date x2 = new Date();
 
+
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(x2);
 		cal.add(Calendar.HOUR_OF_DAY, 4);
-
+		x2 = cal.getTime();
+		
 		System.out.println("Data atual x1: " + SDF1.format(x1));
+		System.out.println();
+		System.out.println("4 horas a mais do horário local x2: " + SDF1.format(x2));
 
 	}
 
